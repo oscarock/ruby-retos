12ddf5872c96c85190fe731e546a58e973059df9
Oscar Felipe Diaz Lozano <oscar.diaz@covinoc.local>
2017-06-07 11:37:23 -0500

    reto 5 terminado

--- retos/5-iterando-array.rb
+++ retos/5-iterando-array.rb
@@ -1,27 +1,7 @@
-def findIndex(values, target)
-  values.each_with_index do |value, i|
-    puts i if value == target
-  end
+def count_matches(array=[], num1)
+    puts array.count(num1)
 end
 
-findIndex([1,2,3], 3)
-
-# def count_matches(arr = [],num1)
-#     cont = arr.count
-#     # puts cont
-#     arr.each do |elem,num1|
-#         puts num1
-#     end
-# end
-#
-# count_matches([1,2,2,3],2)
-
-# def count_matches(arr = [],num1)
-#     result = arr.inject({}) do |hash, key|
-#
-#         hash.include?(key) ? hash[key] += 1 : hash[key] = 1
-#         hash
-#     end
-# end
-
-# puts count_matches([1,2,2,3],2)
+count_matches([1], 1)
+count_matches([3,2,3], 3)
+count_matches([1], 3)
