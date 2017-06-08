298c9247e6bdcb6a2150ac13d9e4378148718f75
Oscar Felipe Diaz Lozano <oscar.diaz@covinoc.local>
2017-06-08 17:23:26 -0500

    reto 8 temporal

--- retos/8-ajustando-texto.rb
+++ retos/8-ajustando-texto.rb
@@ -1,15 +1,11 @@
-# La función debe ajustar el texto a la longitud que se especifique en el segundo parámetro.
-# Si la longitud de la cadena es mayor que el valor
-# del segundo parámetro, se debe cortar.
-# de lo contrario, se debe completar con espacios.
-# adjust_text("", 3) #=> "   "
-# adjust_text("Hola", 0) #=> ""
-# adjust_text("Hola", 10) #=> "Hola      "
-
 def adjust_text(str,num1)
-    puts str.split(//, 3)
-
-
+    if str.length > num1
+        p  str.slice(0,num1)
+    else
+        p str << " " * num1
+    end
 end
 
-adjust_text("hola mundo",3)
+adjust_text("", 3) #=> "   "
+adjust_text("Hola", 0) #=> ""
+adjust_text("Hola", 10) #=> "Hola      "
