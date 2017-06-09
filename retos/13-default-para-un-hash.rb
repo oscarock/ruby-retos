e641919c20fc80e14d01de588f5847b9cce37e9e
Your Name <you@example.com>
2017-06-09 15:39:58 -0500

    cambios

--- retos/13-default-para-un-hash.rb
+++ retos/13-default-para-un-hash.rb
@@ -1,9 +1,17 @@
 def defaults(hash = {})
-    hash = {:altitude => 4500, :pressure => 234}
-    puts hash = {altitude: 4500, pressure: 234}
-    if hash.has_key?(:temperature)
+    hash = { 23, altitude: 4500, pressure: 234}
+
+    if hash.has_key?(:temperature) == false
         hash[:temperature] = 10
     end
+    p hash
+    # puts hash.has_key?(:temperature)
+
+    # hash = {:altitude => 4500, :pressure => 234}
+    # puts hash = {altitude: 4500, pressure: 234}
+    # if hash.has_key?(:temperature)
+    #     hash[:temperature] = 10
+    # end
 
 end
 
--- retos/16-es-una-placa.rb
+++ retos/16-es-una-placa.rb
@@ -2,9 +2,9 @@
 # un vehículo o no. Las placas de un vehículo se componen de tres letras
 # mayúsculas, seguidas de tres dígitos (p.e. AAA234, o UHJ765).
 
-#######solucion#######
+####### mi solucion #######
 def is_plate?(string)
-    expression = /^[A-Z]{3}[0-9]{3}$/.match(string)
+    expression = /[A-Z]{3}[0-9]{3}/.match(string)
 
     if expression != nil
         p true
@@ -14,3 +14,9 @@ def is_plate?(string)
 end
 
 is_plate?("ALS546")
+
+
+####### solucion plataforma #######
+def is_plate?(string)
+  string =~ /^[A-Z]{3}[0-9]{3}$/
+end
