fe4f8af3beb3e84f6bb324b64aa125b8b0305c01
oscar diaz <you@example.com>
2017-06-18 16:50:58 -0500

    reto 22 ruby ok :)

---
+++ retos/22-evitando-colisiones.rb
@@ -0,0 +1,17 @@
+# definir dos módulos de Ruby llamados System y Sales que contengan, 
+# cada uno, una clase llamada Record.
+# El evaluador intentará crear una instancia de cada una de 
+# estas clases de la siguiente forma:
+
+# system_record = System::Record.new
+# sales_record = Sales::Record.new
+
+module System
+	class Record
+	end
+end
+
+module Sales
+	class Record
+	end
+end
