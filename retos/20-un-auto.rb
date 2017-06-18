7106fa9d3bc96b23b47b77bca19fddb592e48518
oscar diaz <you@example.com>
2017-06-18 16:20:19 -0500

    reto 20 ruby ok :)

--- retos/20-un-auto.rb
+++ retos/20-un-auto.rb
@@ -1,68 +1,35 @@
-class Car
+class Car 
     def initialize()
         @accelerate = 0
-        @brake = 0
         @velocity = 0
-    end
-
-    #Getter
-    def accelerate(accelerate = 0)
-        @velocity = @accelerate += 1
-    end
-
-    def brake(accelerate = 0)
-        @brake = @velocity -= 1
+        @brake = 0
     end
 
     def velocity
         @velocity
     end
 
-    # Setters
-    def accelerate=(new_accelerate = 0)
-        # puts "accelerate act: #{@accelerate}"
-        # puts "velocidad #{@velocity}"
-        puts "total ------> #{@velocity = @velocity + new_accelerate}"
-        # @velocity = @velocity + new_accelerate
-        # puts "velocidad act: #{new_accelerate}"
-        # @velocity = @accelerate + new_accelerate
-        # @accelerate = @velocity
+    def accelerate(new_accelerate = 1)
+        @velocity = @accelerate += new_accelerate
     end
 
-    def brake=(num_break = 0)
-        # @velocity = @break - num_break
-        @velocity = @velocity - num_break
+    def brake(new_brake = 1)
+        @brake = @velocity -= new_brake
     end
 end
 
 car = Car.new
 
-# puts car.velocity
-car.accelerate
-car.accelerate
-car.accelerate
-car.accelerate = 2
-car.accelerate
-#
-puts car.velocity
-# car.accelerate
-# puts car.velocity
-# car.accelerate
-# car.accelerate = 2
-# puts car.accelerate
+puts car.velocity # => 0
 
-# puts car.velocity
-#
-# car.accelerate
-# puts car.velocity # => 1
-#
-# car.accelerate = 2
-# puts car.velocity # => 3
-#
-# car.brake
-# puts car.velocity # => 2
-#
-# car.brake = 2
-# puts car.velocity # => 0
+car.accelerate
+puts car.velocity # => 1
 
-# car.velocity = 200
+car.accelerate(2) 
+puts car.velocity # => 3
+
+car.brake
+puts car.velocity # => 2
+
+car.brake(2)
+puts car.velocity # => 0
