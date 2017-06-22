f0f639ef82cadb518193d1957000bae2678e3404
Your Name <you@example.com>
2017-06-22 16:49:45 -0500

    reto 23 ruby ok :)

--- retos/23-incluyendo-un-modulo.rb
+++ retos/23-incluyendo-un-modulo.rb
@@ -1,32 +1,63 @@
+# crear dos clases, una llamada Article y la otra llamada Comment que tengan
+# un atributo body y que incluyan el módulo TextAnalyzer.
+# Las clases se deben poder utilizar de la siguiente forma
+# (fíjate que el cuerpo se pasa como argumento del constructor):
+
+####### mi solucion #######
+module TextAnalyzer
+  def num_words
+    @body ? @body.split.size : 0
+  end
+
+  def num_chars
+    @body ? @body.chars.size : 0
+  end
+end
+
 class Article
-  include TextAnalyzer
+include TextAnalyzer
   def initialize(body)
   	@body = body
+  end
+  def body
+    @body
   end
 end
 
 class Comment
   include TextAnalyzer
   def initialize(body)
+    @body = body
+  end
+  def body
+    @body
   end
 end
-
-
-module TextAnalyzer
-  def num_words
-    @body ? @body.split.size : 0
-  end
-  
-  def num_chars
-    @body ? @body.chars.size : 0
-  end
-end
-
 
 a1 = Article.new("Este es el cuerpo del artículo")
-a1.num_words #=> 6
+p a1.num_words #=> 6
 a1.num_chars #=> 30
 
 c1 = Comment.new("Este es un comentario")
 c1.num_words #=> 4
 c1.num_chars #=> 21
+
+
+####### solucion plataforma #######
+class Article
+  attr_reader :body
+  include TextAnalyzer
+
+  def initialize(body)
+    @body = body
+  end
+end
+
+class Comment
+  attr_reader :body
+  include TextAnalyzer
+
+  def initialize(body)
+    @body = body
+  end
+end
